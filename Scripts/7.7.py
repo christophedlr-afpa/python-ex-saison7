bf495558341379dea906716c1106954382c2c971
Christophe Daloz - De Los Rios <christophedlr@gmail.com>
2020-09-10 09:39:25 +0200

    Exercise 7.7 python algorithm merge two arrays
    Merging two arrays with rational method work in part:
    - Second array is always values lesser than first array
    - Two arrays are identical length

--- Scripts/7.7.py
+++ Scripts/7.7.py
@@ -10,19 +10,26 @@ print("Le second tableau est : ")
 print(arraySecond)
 
 
+# for i in range(0, len(arrayFirst)):
+#     newArray.append(arrayFirst[i])
+#
+# for i in range(0, len(arraySecond)):
+#     newArray.append(arraySecond[i])
+
 for i in range(0, len(arrayFirst)):
+    for j in range(i, len(arraySecond)):
+        if arraySecond[j] < arrayFirst[i]:
+            newArray.append(arraySecond[j])
     newArray.append(arrayFirst[i])
+newArray.append(arraySecond[len(arraySecond)-1])
 
-for i in range(0, len(arraySecond)):
-    newArray.append(arraySecond[i])
-
-for x in range(0, len(newArray)):
-    for j in range(x, len(newArray)):
-        if newArray[j] < newArray[x]:
-            pos = j
-            temp = newArray[pos]
-            newArray[pos] = newArray[x]
-            newArray[x] = temp
+# for x in range(0, len(newArray)):
+#     for j in range(x, len(newArray)):
+#         if newArray[j] < newArray[x]:
+#             pos = j
+#             temp = newArray[pos]
+#             newArray[pos] = newArray[x]
+#             newArray[x] = temp
 
 print("Le troisième tableau est : ")
 print(newArray)
